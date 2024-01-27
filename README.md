3dc58411c9286f98a0f6f721437964aed082c1d2
Thomas Broyer <t.broyer@ltgt.net>
2024-01-27 16:37:31 +0100

    Add initial implementation

---
+++ README.md
@@ -0,0 +1,68 @@
+# decorator-compose
+
+This library exposes a helper function to create a composed decorator.
+Only standard decorators are supported, not TypeScript _experimental_ decorators.
+
+## Use-case
+
+There are cases where you'd want to apply always the same set of decorators together to a class or class member.
+Rather than make sure you always follow that rule in your files (I suppose you could write an ESLint rule for that),
+you can instead declare a _composed decorator_ that, well, composes that set of decorators and applies them whenever the composed decorator is applied.
+
+This means that instead of:
+
+```js
+class Foo {
+  @dec1 @dec2("option") @dec3 accessor bar;
+}
+```
+
+you could write:
+
+```js
+class Foo {
+  @composedDecorator accessor bar;
+}
+```
+
+or
+
+```js
+class Foo {
+  @composedDecorator("option") accessor bar;
+}
+```
+
+with the exact same behavior.
+
+## Usage
+
+First, import the `compose` function:
+
+```js
+import { compose } from "decorator-compose";
+```
+
+The function takes a list of decorators and returns a decorator.
+
+For a decorator with no argument, the returned decorator can be stored in a variable:
+
+```js
+const composedDecorator = compose(dec1, dec2("option"), dec3);
+
+class Foo {
+  @composedDecorator accessor bar;
+}
+```
+
+For a decorator with arguments, create a function that returns the composed decorator:
+
+```js
+function composedDecorator(option) {
+  return composed(dec1, dec2(option), dec3);
+}
+
+class Foo {
+  @composedDecorator("option") accessor bar;
+}
+```
---
+++ index.js
@@ -0,0 +1,68 @@
+export function compose(...decorators) {
+  return function (value, context) {
+    let kind = context.kind;
+    let newValue = kind === "accessor" ? { ...value } : value;
+    let initializers = [];
+    for (let i = decorators.length - 1; i >= 0; i--) {
+      let result = decorators[i].call(this, newValue, cloneContext(context));
+      if (result === undefined) continue;
+      switch (kind) {
+        case "field":
+          initializers.push(result);
+          break;
+        case "accessor":
+          if (result === null || typeof result !== "object") {
+            throw new TypeError("Object expected");
+          }
+          {
+            let { get, set, init } = result;
+            if (get !== undefined) newValue.get = assertCallable(get);
+            if (set !== undefined) newValue.set = assertCallable(set);
+            if (init !== undefined) initializers.push(assertCallable(init));
+          }
+          break;
+        default:
+          newValue = assertCallable(result);
+      }
+    }
+    let runInitializers;
+    switch (initializers.length) {
+      case 0:
+        break;
+      case 1:
+        runInitializers = initializers[0];
+        break;
+      default:
+        runInitializers = function (initialValue) {
+          for (let i = initializers.length - 1; i >= 0; i--) {
+            initialValue = initializers[i].call(this, initialValue);
+          }
+          return initialValue;
+        };
+    }
+    switch (kind) {
+      case "field":
+        return runInitializers;
+      case "accessor":
+        if (runInitializers) {
+          newValue.init = runInitializers;
+        }
+      // eslint-disable-next-line no-fallthrough
+      default:
+        return newValue;
+    }
+  };
+}
+
+function assertCallable(f) {
+  if (typeof f !== "function") {
+    throw new TypeError("Function expected");
+  }
+  return f;
+}
+
+function cloneContext(ctx) {
+  ctx = { ...ctx };
+  if (ctx.access) ctx.access = { ...ctx.access };
+  return ctx;
+}
